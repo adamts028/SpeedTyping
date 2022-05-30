41e86498ccad1210a634a7bbfca171cab67161df
adamts028 <adamts028@gmail.com>
2022-05-31 11:02:06 +1200

    java progress and added time/wpm

--- speedtyping/base/static/java/test.js
+++ speedtyping/base/static/java/test.js
@@ -1,23 +1,25 @@
     let text_box = document.querySelector("#text-box")
     let input_box = document.querySelector("#input-box");
     let progress = document.querySelector("#progress")
+    let time_count = document.querySelector("#timer")
+    let wpm_count = document.querySelector("#wpm-count")
+
     let time = 0;
+    let timer = null;
     let wpm = 0;
     let accuracy = 0;
     let errors = 0;
+    let total_errors = 0;
     let chars_typed = 0;
 
+
+
 function start() {
-    //variable initialisation
+    //to be changed
     let text = "This is a text that you need to type quickly."
-    time = 0;
-    wpm = 0;
-    accuracy = 0;
-    errors = 0;
-    chars_typed = 0;
 
     clearInterval(timer);
-    timer = setInterval(updateTimer, 1000);
+    //timer = setInterval(updateTimer, 1000);
 
     //Add text
     text_box.textContent = null;
@@ -30,14 +32,16 @@ function start() {
 
 }
 
+
 function take_input() {
-    input = input_box.value;
-    input_array = input.split('');
+    curr_input = input_box.value;
+    input_array = curr_input.split('');
+
     chars_typed++;
 
     quote_array = quote_text.querySelectorAll('span');
     quote_array.forEach((char, index) => {
-    let typed_char = input_array[index]
+    let typed_char =  [index]
 
         // characters not currently typed
     if (typed_char == null) {
@@ -56,4 +60,8 @@ function take_input() {
 
       // increment number of errors
       errors++;
+      }
+});
+
+
 }
