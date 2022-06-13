b0fe7d3fbdafb62ed5661bbfa931134e57ecd65b
LukeNZL <lukeyoobee@gmail.com>
2022-06-14 11:29:49 +1200

    more scoreboard work

--- speedtyping/base/admin.py
+++ speedtyping/base/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Score
 
+
+
 # registers score in the admin database so admins can see/modify results
 admin.site.register(Score)
--- speedtyping/base/models.py
+++ speedtyping/base/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # class for storing attributes of a users score
 class Score(models.Model):
@@ -8,6 +10,9 @@ class Score(models.Model):
     accuracy = models.FloatField(default=0)
 
     created = models.DateTimeField(auto_now_add=True)
+
+
+
     # allows the scoreboard to display in descending order from the largest to the smallest typing speed
     class Meta:
         ordering = ['-typing_speed']
